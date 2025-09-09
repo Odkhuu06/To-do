b06b6e49ec7dc5bda81c76cc1d06debfe95b6a57
25LP2339 25LP2339 <25LP2339@admins-iMac.local>
2025-09-09 00:26:04 -0700

    Add missing components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { Filter } from "@/components/Filter";
-import { Form } from "@/components/Form";
-import { Task } from "@/components/Task";
+import { Form } from "@/components/form";
+import { Task } from "@/components/task";
 import { useState } from "react";
 
 export default function Home() {
